7e2f42d3bb486825d94c122040a4c63928711ef5
mohit-singh-13 <mohitsingh092003@gmail.com>
2024-07-03 06:40:33 +0530

    Add some tailwind config

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -57,12 +57,12 @@ const Chat = () => {
     return (
         <div className="bg-gray-300 h-screen w-11/12 max-w-[1210px] mx-auto relative max-h-max">
 
-            <div className="h-full">
+            <div className="h-full max-sm:mt-[5rem]">
                 <Messages messages={message} loader={loader}/>
             </div>
 
-            <div className="flex gap-2 fixed bottom-5 w-[79%]">
-                <input type="text" name="messages" id="messages" placeholder="type your message" className="w-[95%] placeholder:text-[1.1rem] p-5 rounded-lg border border-black" onChange={changeHandler} onKeyDown={keyHandler} value={text}/>
+            <div className="flex gap-2 fixed bottom-5 w-11/12 max-w-[1210px] justify-center">
+                <input type="text" name="messages" id="messages" placeholder="type your message" className="w-full placeholder:text-[1.1rem] p-5 rounded-lg border border-black" onChange={changeHandler} onKeyDown={keyHandler} value={text}/>
                 <button className="px-[2rem] py-[0.7rem] bg-blue-500 rounded-lg font-bold text-white" onClick={clickHandler}>Send</button>
             </div>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'max-sm': { 'max': '450px' },
+      }
+    },
   },
   plugins: [],
 }
